b24971f877a78b2e9d6df84706c1e74467345c78
Furkan Ilgenci <furkanilgenci@gmail.com>
2021-03-27 15:55:40 +0300

    Add mock authenticated user

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const User = require('./models/user')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -21,6 +22,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(async (req, res, next) => {
+  req.user = await User.findById('605f1f2cf126e00141cafc0d')
+
+  next()
+})
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
