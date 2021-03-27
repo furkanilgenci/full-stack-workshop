9554506d84dc00d297625ebf8531662bcb42f54c
Furkan Ilgenci <furkanilgenci@gmail.com>
2021-03-27 18:03:15 +0300

    Implement attend to course on frontend

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,34 @@ import { createStore } from 'vuex'
 import axios from 'axios'
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    user: {
+      // Will be filled when logged in, after authentication is implemented
+      _id: '605f1f2cf126e00141cafc0d',
+      // ...
+    },
+    courses: null,
+  },
+  mutations: {
+    SET_COURSES(state, courses) {
+      state.courses = courses
+    },
+  },
   actions: {
     async fetchUsers() {
       return (await axios.get('/api/users')).data
+    },
+    async attendToCourse({ state, dispatch }, courseId) {
+      await axios.post(`/api/users/${state.user._id}/attended-courses`, {
+        _id: courseId,
+      })
+
+      await dispatch('fetchCourses')
+    },
+    async fetchCourses({ commit }) {
+      const courses = (await axios.get('/api/courses')).data
+
+      commit('SET_COURSES', courses)
     },
   },
   modules: {},
--- frontend/src/views/courses.vue
+++ frontend/src/views/courses.vue
@@ -1,10 +1,35 @@
 <script>
-export default {}
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  async created() {
+    await this.fetchCourses()
+  },
+  computed: {
+    ...mapState(['courses']),
+  },
+  methods: {
+    ...mapActions(['fetchCourses', 'attendToCourse']),
+  },
+}
 </script>
 
 <template lang="pug">
-h1 HEY
-
+div(v-if="courses")
+  h4 There are {{ courses.length }} course(s)
+  ul
+    li.course-card(v-for="course in courses")
+      h3 Name: {{ course.name }}
+      h4 Price: {{ course.price }}
+      h4 Participants:
+      ul(v-for="participant in course.participants")
+        li
+          h5 Name: {{ participant.name }}
+      button(@click="attendToCourse(course._id)") Attend
 </template>
 
-<style></style>
+<style>
+.course-card {
+  border: 1px solid black;
+}
+</style>
